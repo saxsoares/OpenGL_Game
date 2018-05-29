563cb209b9d6506ca7cf74178a729bee21dc1e46
Fernando Soares <sax.soares@gmail.com>
2018-05-29 18:55:01 -0300

    First

---
+++ carro.c
@@ -0,0 +1,362 @@
+#include<GL/glut.h>
+
+void DesenhaCarro(){
+    glPushMatrix();
+        //Roda dianteira esquerda
+        glColor3f(0.1, 0.1, 0.1);
+        glBegin(GL_QUADS);
+            glVertex3f(-8.5, 0.0, -31.8); glVertex3f(-5.5, 0.0, -31.8);
+            glVertex3f(-5.5, 0.0, -34.2); glVertex3f(-8.5, 0.0, -34.2);
+
+            glVertex3f(-8.5, 0.0, -34.2); glVertex3f(-5.5, 0.0, -34.2);
+            glVertex3f(-5.5, 1.7, -35.9); glVertex3f(-8.5, 1.7, -35.9);
+
+            glVertex3f(-8.5, 1.7, -35.9); glVertex3f(-5.5, 1.7, -35.9);
+            glVertex3f(-5.5, 4.2, -35.9); glVertex3f(-8.5, 4.2, -35.9);
+
+            glVertex3f(-8.5, 4.2, -35.9); glVertex3f(-5.5, 4.2, -35.9);
+            glVertex3f(-5.5, 5.9, -34.2); glVertex3f(-8.5, 5.9, -34.2);
+
+            glVertex3f(-8.5, 5.9, -34.2); glVertex3f(-5.5, 5.9, -34.2);
+            glVertex3f(-5.5, 5.9, -31.8); glVertex3f(-8.5, 5.9, -31.8);
+
+            glVertex3f(-8.5, 5.9, -31.8); glVertex3f(-5.5, 5.9, -31.8);
+            glVertex3f(-5.5, 4.2, -30.1); glVertex3f(-8.5, 4.2, -30.1);
+
+            glVertex3f(-8.5, 4.2, -30.1); glVertex3f(-5.5, 4.2, -30.1);
+            glVertex3f(-5.5, 1.7, -30.1); glVertex3f(-8.5, 1.7, -30.1);
+
+            glVertex3f(-8.5, 1.7, -30.1); glVertex3f(-5.5, 1.7, -30.1);
+            glVertex3f(-5.5, 0.0, -31.8); glVertex3f(-8.5, 0.0, -31.8);
+        glEnd();
+            //"Tampa" esquerda
+            glBegin(GL_POLYGON);
+                glVertex3f(-8.5, 0.0, -31.8); glVertex3f(-8.5, 0.0, -34.2);
+                glVertex3f(-8.5, 1.7, -35.9); glVertex3f(-8.5, 4.2, -35.9);
+                glVertex3f(-8.5, 5.9, -34.2); glVertex3f(-8.5, 5.9, -31.8);
+                glVertex3f(-8.5, 4.2, -30.1); glVertex3f(-8.5, 1.7, -30.1);
+            glEnd();
+
+        //Roda dianteira direita
+        glColor3f(0.1, 0.1, 0.1);
+        glBegin(GL_QUADS);
+            glVertex3f(5.5, 0.0, -31.8); glVertex3f(8.5, 0.0, -31.8);
+            glVertex3f(8.5, 0.0, -34.2); glVertex3f(5.5, 0.0, -34.2);
+
+            glVertex3f(5.5, 0.0, -34.2); glVertex3f(8.5, 0.0, -34.2);
+            glVertex3f(8.5, 1.7, -35.9); glVertex3f(5.5, 1.7, -35.9);
+
+            glVertex3f(5.5, 1.7, -35.9); glVertex3f(8.5, 1.7, -35.9);
+            glVertex3f(8.5, 4.2, -35.9); glVertex3f(5.5, 4.2, -35.9);
+
+            glVertex3f(5.5, 4.2, -35.9); glVertex3f(8.5, 4.2, -35.9);
+            glVertex3f(8.5, 5.9, -34.2); glVertex3f(5.5, 5.9, -34.2);
+
+            glVertex3f(5.5, 5.9, -34.2); glVertex3f(8.5, 5.9, -34.2);
+            glVertex3f(8.5, 5.9, -31.8); glVertex3f(5.5, 5.9, -31.8);
+
+            glVertex3f(5.5, 5.9, -31.8); glVertex3f(8.5, 5.9, -31.8);
+            glVertex3f(8.5, 4.2, -30.1); glVertex3f(5.5, 4.2, -30.1);
+
+            glVertex3f(5.5, 4.2, -30.1); glVertex3f(8.5, 4.2, -30.1);
+            glVertex3f(8.5, 1.7, -30.1); glVertex3f(5.5, 1.7, -30.1);
+
+            glVertex3f(5.5, 1.7, -30.1); glVertex3f(8.5, 1.7, -30.1);
+            glVertex3f(8.5, 0.0, -31.8); glVertex3f(5.5, 0.0, -31.8);
+        glEnd();
+            //"Tampa" direita
+            glBegin(GL_POLYGON);
+                glVertex3f(8.5, 0.0, -31.8); glVertex3f(8.5, 0.0, -34.2);
+                glVertex3f(8.5, 1.7, -35.9); glVertex3f(8.5, 4.2, -35.9);
+                glVertex3f(8.5, 5.9, -34.2); glVertex3f(8.5, 5.9, -31.8);
+                glVertex3f(8.5, 4.2, -30.1); glVertex3f(8.5, 1.7, -30.1);
+            glEnd();
+
+        //Roda traseira esquerda
+        glColor3f(0.1, 0.1, 0.1);
+        glBegin(GL_QUADS);
+            glVertex3f(-10.0, 0.0, -5.5);  glVertex3f(-6.0, 0.0, -5.5);
+            glVertex3f(-6.0, 0.0, -8.5);   glVertex3f(-10.0, 0.0, -8.5);
+
+            glVertex3f(-10.0, 0.0, -8.5);  glVertex3f(-6.0, 0.0, -8.5);
+            glVertex3f(-6.0, 2.1, -10.5);  glVertex3f(-10.0, 2.1, -10.5);
+
+            glVertex3f(-10.0, 2.1, -10.5); glVertex3f(-6.0, 2.1, -10.5);
+            glVertex3f(-6.0, 5.0, -10.5);  glVertex3f(-10.0, 5.0, -10.5);
+
+            glVertex3f(-10.0, 5.0, -10.5); glVertex3f(-6.0, 5.0, -10.5);
+            glVertex3f(-6.0, 7.1, -8.5);   glVertex3f(-10.0, 7.1, -8.5);
+
+            glVertex3f(-10.0, 7.1, -8.5);  glVertex3f(-6.0, 7.1, -8.5);
+            glVertex3f(-6.0, 7.1, -5.5);   glVertex3f(-10.0, 7.1, -5.5);
+
+            glVertex3f(-10.0, 7.1, -5.5);  glVertex3f(-6.0, 7.1, -5.5);
+            glVertex3f(-6.0, 5.0, -3.5);   glVertex3f(-10.0, 5.0, -3.5);
+
+            glVertex3f(-10.0, 5.0, -3.5);  glVertex3f(-6.0, 5.0, -3.5);
+            glVertex3f(-6.0, 2.1, -3.5);   glVertex3f(-10.0, 2.1, -3.5);
+
+            glVertex3f(-10.0, 2.1, -3.5);  glVertex3f(-6.0, 2.1, -3.5);
+            glVertex3f(-6.0, 0.0, -5.5);   glVertex3f(-10.0, 0.0, -5.5);
+        glEnd();
+            //"Tampa" esquerda
+            glBegin(GL_POLYGON);
+                glVertex3f(-10.0, 0.0, -5.5);  glVertex3f(-10.0, 0.0, -8.5);
+                glVertex3f(-10.0, 2.1, -10.5); glVertex3f(-10.0, 5.0, -10.5);
+                glVertex3f(-10.0, 7.1, -8.5);  glVertex3f(-10.0, 7.1, -5.5);
+                glVertex3f(-10.0, 5.0, -3.5);  glVertex3f(-10.0, 2.1, -3.5);
+            glEnd();
+
+        //Roda traseira direita
+        glBegin(GL_QUADS);
+            glVertex3f(6.0, 0.0, -5.5);   glVertex3f(10.0, 0.0, -5.5);
+            glVertex3f(10.0, 0.0, -8.5);  glVertex3f(6.0, 0.0, -8.5);
+
+            glVertex3f(6.0, 0.0, -8.5);   glVertex3f(10.0, 0.0, -8.5);
+            glVertex3f(10.0, 2.1, -10.5); glVertex3f(6.0, 2.1, -10.5);
+
+            glVertex3f(6.0, 2.1, -10.5);  glVertex3f(10.0, 2.1, -10.5);
+            glVertex3f(10.0, 5.0, -10.5); glVertex3f(6.0, 5.0, -10.5);
+
+            glVertex3f(6.0, 5.0, -10.5);  glVertex3f(10.0, 5.0, -10.5);
+            glVertex3f(10.0, 7.1, -8.5);  glVertex3f(6.0, 7.1, -8.5);
+
+            glVertex3f(6.0, 7.1, -8.5);   glVertex3f(10.0, 7.1, -8.5);
+            glVertex3f(10.0, 7.1, -5.5);  glVertex3f(6.0, 7.1, -5.5);
+
+            glVertex3f(6.0, 7.1, -5.5);   glVertex3f(10.0, 7.1, -5.5);
+            glVertex3f(10.0, 5.0, -3.5);  glVertex3f(6.0, 5.0, -3.5);
+
+            glVertex3f(6.0, 5.0, -3.5);   glVertex3f(10.0, 5.0, -3.5);
+            glVertex3f(10.0, 2.1, -3.5);  glVertex3f(6.0, 2.1, -3.5);
+
+            glVertex3f(6.0, 2.1, -3.5);   glVertex3f(10.0, 2.1, -3.5);
+            glVertex3f(10.0, 0.0, -5.5);  glVertex3f(6.0, 0.0, -5.5);
+        glEnd();
+            //"Tampa" direita
+            glBegin(GL_POLYGON);
+                glVertex3f(10.0, 0.0, -5.5);  glVertex3f(10.0, 0.0, -8.5);
+                glVertex3f(10.0, 2.1, -10.5); glVertex3f(10.0, 5.0, -10.5);
+                glVertex3f(10.0, 7.1, -8.5);  glVertex3f(10.0, 7.1, -5.5);
+                glVertex3f(10.0, 5.0, -3.5);  glVertex3f(10.0, 2.1, -3.5);
+            glEnd();
+
+        //Laterais
+        glColor3f(1.0, 0.0 , 0.0);
+        glBegin(GL_TRIANGLES);
+            //Esquerda
+            glVertex3f(-8.5, 5.4, 0.0);   glVertex3f(-8.5, 5.4, -2.5);   glVertex3f(-8.5, 11.6, 0.0);//01
+            glVertex3f(-8.5, 5.4, -2.5);  glVertex3f(-8.5, 8.1, -5.1);   glVertex3f(-8.5, 11.6, 0.0);//02
+            glVertex3f(-8.5, 8.1, -5.1);  glVertex3f(-8.5, 16.2, -10.0); glVertex3f(-8.5, 11.6, 0.0);//03
+            glVertex3f(-8.5, 8.1, -5.1);  glVertex3f(-8.5, 8.1, -8.9);   glVertex3f(-8.5, 16.2, -10.0);//04
+            glVertex3f(-8.5, 8.1, -8.9);  glVertex3f(-8.5, 16.2, -20.0); glVertex3f(-8.5, 16.2, -10.0);//05
+            glVertex3f(-8.5, 8.1, -8.9);  glVertex3f(-8.5, 11.8, -26.7);  glVertex3f(-8.5, 16.2, -20.0);//06
+            glVertex3f(-8.5, 8.1, -8.9);  glVertex3f(-8.5, 5.4, -11.5);  glVertex3f(-8.5, 11.8, -26.7);//07
+            glVertex3f(-8.5, 5.4, -11.5); glVertex3f(-8.5, 6.6, -31.5);  glVertex3f(-8.5, 11.8, -26.7);//08
+            glVertex3f(-8.5, 5.4, -11.5); glVertex3f(-8.5, 4.5, -29.3);  glVertex3f(-8.5, 6.6, -31.5);//09
+            glVertex3f(-8.5, 5.4, -11.5); glVertex3f(-8.5, 2.9, -11.5);  glVertex3f(-8.5, 4.5, -29.3);//10
+            glVertex3f(-8.5, 2.9, -11.5); glVertex3f(-8.5, 2.9, -29.3);  glVertex3f(-8.5, 4.5, -29.3);//11
+            glVertex3f(-8.5, 6.6, -31.5); glVertex3f(-8.5, 6.6, -34.5);  glVertex3f(-8.5, 11.8, -26.7);//12
+            glVertex3f(-8.5, 6.6, -34.5); glVertex3f(-8.5, 9.6, -40.0);  glVertex3f(-8.5, 11.8, -26.7);//13
+            glVertex3f(-8.5, 6.6, -34.5); glVertex3f(-8.5, 4.5, -36.7);  glVertex3f(-8.5, 9.6, -40.0);//14
+            glVertex3f(-8.5, 4.5, -36.7); glVertex3f(-8.5, 4.5, -40.0);  glVertex3f(-8.5, 9.6, -40.0);//15
+            //Direita
+            glVertex3f(8.5, 5.4, 0.0);   glVertex3f(8.5, 5.4, -2.5);   glVertex3f(8.5, 11.6, 0.0);//01
+            glVertex3f(8.5, 5.4, -2.5);  glVertex3f(8.5, 8.1, -5.1);   glVertex3f(8.5, 11.6, 0.0);//02
+            glVertex3f(8.5, 8.1, -5.1);  glVertex3f(8.5, 16.2, -10.0); glVertex3f(8.5, 11.6, 0.0);//03
+            glVertex3f(8.5, 8.1, -5.1);  glVertex3f(8.5, 8.1, -8.9);   glVertex3f(8.5, 16.2, -10.0);//04
+            glVertex3f(8.5, 8.1, -8.9);  glVertex3f(8.5, 16.2, -20.0); glVertex3f(8.5, 16.2, -10.0);//05
+            glVertex3f(8.5, 8.1, -8.9);  glVertex3f(8.5, 11.8, -26.7);  glVertex3f(8.5, 16.2, -20.0);//06
+            glVertex3f(8.5, 8.1, -8.9);  glVertex3f(8.5, 5.4, -11.5);  glVertex3f(8.5, 11.8, -26.7);//07
+            glVertex3f(8.5, 5.4, -11.5); glVertex3f(8.5, 6.6, -31.5);  glVertex3f(8.5, 11.8, -26.7);//08
+            glVertex3f(8.5, 5.4, -11.5); glVertex3f(8.5, 4.5, -29.3);  glVertex3f(8.5, 6.6, -31.5);//09
+            glVertex3f(8.5, 5.4, -11.5); glVertex3f(8.5, 2.9, -11.5);  glVertex3f(8.5, 4.5, -29.3);//10
+            glVertex3f(8.5, 2.9, -11.5); glVertex3f(8.5, 2.9, -29.3);  glVertex3f(8.5, 4.5, -29.3);//11
+            glVertex3f(8.5, 6.6, -31.5); glVertex3f(8.5, 6.6, -34.5);  glVertex3f(8.5, 11.8, -26.7);//12
+            glVertex3f(8.5, 6.6, -34.5); glVertex3f(8.5, 9.6, -40.0);  glVertex3f(8.5, 11.8, -26.7);//13
+            glVertex3f(8.5, 6.6, -34.5); glVertex3f(8.5, 4.5, -36.7);  glVertex3f(8.5, 9.6, -40.0);//14
+            glVertex3f(8.5, 4.5, -36.7); glVertex3f(8.5, 4.5, -40.0);  glVertex3f(8.5, 9.6, -40.0);//15
+        glEnd();
+
+        //Carroceria
+        glBegin(GL_QUADS);
+            //Parachoque traseiro
+            glColor3f(0.0, 0.0, 0.0);
+            glVertex3f(-8.5, 5.4, 0.0); glVertex3f(8.5, 5.4, 0.0);
+            glVertex3f(8.5, 2.9, 0.0);  glVertex3f(-8.5, 2.9, 0.0);
+
+            glVertex3f(-8.5, 2.9, 0.0); glVertex3f(8.5, 2.9, 0.0);
+            glVertex3f(8.5, 2.9, -2.5); glVertex3f(-8.5, 2.9, -2.5);
+
+            glVertex3f(-8.5, 2.9, -2.5);  glVertex3f(8.5, 2.9, -2.5);
+            glVertex3f(8.5, 5.4, -2.5);   glVertex3f(-8.5, 5.4, -2.5);
+                //Tampa esquerda
+                glVertex3f(-8.5, 2.9, 0.0);  glVertex3f(-8.5, 2.9, -2.5);
+                glVertex3f(-8.5, 5.4, -2.5); glVertex3f(-8.5, 5.4, 0.0);
+                //Tampa direita
+                glVertex3f(8.5, 2.9, 0.0);  glVertex3f(8.5, 2.9, -2.5);
+                glVertex3f(8.5, 5.4, -2.5); glVertex3f(8.5, 5.4, 0.0);
+
+            //Cavidade traseira
+            glColor3f(1.0, 0.0, 0.0);
+            glVertex3f(-8.5, 5.4, -2.5);  glVertex3f(8.5, 5.4, -2.5);
+            glVertex3f(8.5, 8.1, -5.1);   glVertex3f(-8.5, 8.1, -5.1);
+
+            glVertex3f(-8.5, 8.1, -5.1);  glVertex3f(8.5, 8.1, -5.1);
+            glVertex3f(8.5, 8.1, -8.9);   glVertex3f(-8.5, 8.1, -8.9);
+
+            glVertex3f(-8.5, 8.1, -8.9);  glVertex3f(8.5, 8.1, -8.9);
+            glVertex3f(8.5, 8.1, -11.5);  glVertex3f(-8.5, 8.1, -11.5);
+
+            glVertex3f(-8.5, 8.1, -11.5); glVertex3f(8.5, 8.1, -11.5);
+            glVertex3f(8.5, 2.9, -11.5);  glVertex3f(-8.5, 2.9, -11.5);
+
+            //Base
+            glVertex3f(-8.5, 2.9, -11.5); glVertex3f(8.5, 2.9, -11.5);
+            glVertex3f(8.5, 2.9, -29.3);  glVertex3f(-8.5, 2.9, -29.3);
+
+            //Cavidade dianteira
+            glVertex3f(-8.5, 2.9, -29.3); glVertex3f(8.5, 2.9, -29.3);
+            glVertex3f(8.5, 4.5, -29.3);  glVertex3f(-8.5, 4.5, -29.3);
+
+            glVertex3f(-8.5, 4.5, -29.3); glVertex3f(8.5, 4.5, -29.3);
+            glVertex3f(8.5, 6.6, -31.5);  glVertex3f(-8.5, 6.6, -31.5);
+
+            glVertex3f(-8.5, 6.6, -31.5); glVertex3f(8.5, 6.6, -31.5);
+            glVertex3f(8.5, 6.6, -34.5);  glVertex3f(-8.5, 6.6, -34.5);
+
+            glVertex3f(-8.5, 6.6, -34.5); glVertex3f(8.5, 6.6, -34.5);
+            glVertex3f(8.5, 4.5, -36.7);  glVertex3f(-8.5, 4.5, -36.7);
+
+            //Parachoque dianteiro
+            glColor3f(0.0, 0.0, 0.0);
+            glVertex3f(-8.5, 4.5, -36.7); glVertex3f(8.5, 4.5, -36.7);
+            glVertex3f(8.5, 2.9, -36.7);  glVertex3f(-8.5, 2.9, -36.7);
+
+            glVertex3f(-8.5, 2.9, -36.7); glVertex3f(8.5, 2.9, -36.7);
+            glVertex3f(8.5, 2.9, -40.0);  glVertex3f(-8.5, 2.9, -40.0);
+
+            glVertex3f(-8.5, 2.9, -40.0); glVertex3f(8.5, 2.9, -40.0);
+            glVertex3f(8.5, 4.5, -40.0);  glVertex3f(-8.5, 4.5, -40.0);
+                //Tampa esquerda
+                glVertex3f(-8.5, 2.9, -36.7); glVertex3f(-8.5, 2.9, -40.0);
+                glVertex3f(-8.5, 4.5, -40.0); glVertex3f(-8.5, 4.5, -36.7);
+                //Tampa direita
+                glVertex3f(8.5, 2.9, -36.7); glVertex3f(8.5, 2.9, -40.0);
+                glVertex3f(8.5, 4.5, -40.0); glVertex3f(8.5, 4.5, -36.7);
+
+            //Dianteira
+            glColor3f(1.0, 0.0, 0.0);
+            glVertex3f(-8.5, 4.5, -40.0);  glVertex3f(8.5, 4.5, -40.0);
+            glVertex3f(8.5, 9.6, -40.0);   glVertex3f(-8.5, 9.6, -40.0);
+
+            //Capo
+            glVertex3f(-8.5, 9.6, -40.0);  glVertex3f(8.5, 9.6, -40.0);
+            glVertex3f(8.5, 11.8, -26.7);   glVertex3f(-8.5, 11.8, -26.7);
+
+            //Parabrisa
+            glVertex3f(-8.5, 11.8, -26.7);  glVertex3f(8.5, 11.8, -26.7);
+            glVertex3f(8.5, 16.2, -20.0);  glVertex3f(-8.5, 16.2, -20.0);
+
+            //Teto
+            glVertex3f(-8.5, 16.2, -20.0); glVertex3f(8.5, 16.2, -20.0);
+            glVertex3f(8.5, 16.2, -10.0);  glVertex3f(-8.5, 16.2, -10.0);
+
+            //Traseira
+            glVertex3f(-8.5, 16.2, -10.0); glVertex3f(8.5, 16.2, -10.0);
+            glVertex3f(8.5, 11.6, 0.0);   glVertex3f(-8.5, 11.6, 0.0);
+
+            glVertex3f(-8.5, 11.6, 0.0);  glVertex3f(8.5, 11.6, 0.0);
+            glVertex3f(8.5, 5.4, 0.0);    glVertex3f(-8.5, 5.4, 0.0);
+        glEnd();
+
+        //Vidros
+        glColor3f(0.2, 0.2, 0.2);
+            glBegin(GL_QUADS);
+                //Traseiro
+                glVertex3f(-7.5, 12.45, -1.8); glVertex3f(7.5, 12.45, -1.8);
+                glVertex3f(7.5, 15.8, -9.1);   glVertex3f(-7.5, 15.8, -9.1);
+            glEnd();
+            glBegin(GL_QUADS);
+                //Dianteiro
+                glVertex3f(-7.5, 12.7, -25.4); glVertex3f(7.5, 12.7, -25.4);
+                glVertex3f(7.5, 15.7, -20.8);   glVertex3f(-7.5, 15.7, -20.8);
+            glEnd();
+
+            glBegin(GL_POLYGON);
+                //Vidros esquerdo
+                glVertex3f(-8.51, 11.2, -1.6);  glVertex3f(-8.51, 11.2, -25.7);
+                glVertex3f(-8.51, 15.2, -19.7); glVertex3f(-8.51, 15.2, -10.2);
+            glEnd();
+            glBegin(GL_POLYGON);
+                //Vidros direito
+                glVertex3f(8.51, 11.2, -1.6);  glVertex3f(8.51, 11.2, -25.7);
+                glVertex3f(8.51, 15.2, -19.7); glVertex3f(8.51, 15.2, -10.2);
+            glEnd();
+
+        //Portas
+        glColor3f(0.0, 0.0, 0.0);
+            //Esquerda
+            glBegin(GL_LINES);
+                glVertex3f(-8.51, 4.4, -13.0); glVertex3f(-8.51, 15.2, -13.0);
+                glVertex3f(-8.51, 4.4, -13.0); glVertex3f(-8.51, 4.4, -25.7);
+                glVertex3f(-8.51, 4.4, -25.7); glVertex3f(-8.51, 11.2, -25.7);
+            glEnd();
+            //Direita
+            glBegin(GL_LINES);
+                glVertex3f(8.51, 4.4, -13.0); glVertex3f(8.51, 15.2, -13.0);
+                glVertex3f(8.51, 4.4, -13.0); glVertex3f(8.51, 4.4, -25.7);
+                glVertex3f(8.51, 4.4, -25.7); glVertex3f(8.51, 11.2, -25.7);
+            glEnd();
+
+        //Lanternas
+        glColor3f(0.8, 0.0, 0.0);
+            //Traseira esquerda
+            glBegin(GL_QUADS);
+                glVertex3f(-6.5, 9.1, 0.1);  glVertex3f(-4.5, 9.1, 0.1);
+                glVertex3f(-4.5, 10.6, 0.1); glVertex3f(-6.5, 10.6, 0.1);
+            glEnd();
+
+            //Traseira direita
+            glBegin(GL_QUADS);
+                glVertex3f(6.5, 9.1, 0.1);  glVertex3f(4.5, 9.1, 0.1);
+                glVertex3f(4.5, 10.6, 0.1); glVertex3f(6.5, 10.6, 0.1);
+            glEnd();
+
+        glColor3f(0.9, 0.9, 0.0);
+            //Seta esquerda
+            glBegin(GL_QUADS);
+                glVertex3f(-7.5, 9.1, 0.1);  glVertex3f(-6.5, 9.1, 0.1);
+                glVertex3f(-6.5, 10.6, 0.1); glVertex3f(-7.5, 10.6, 0.1);
+            glEnd();
+
+            //Seta direita
+            glBegin(GL_QUADS);
+                glVertex3f(6.5, 9.1, 0.1);  glVertex3f(7.5, 9.1, 0.1);
+                glVertex3f(7.5, 10.6, 0.1); glVertex3f(6.5, 10.6, 0.1);
+            glEnd();
+
+        //Escapamentos
+        glColor3f(0.5, 0.5, 0.5);
+        glBegin(GL_LINES);
+            glVertex3f(5.3, 3.9, 0.1); glVertex3f(5.7, 3.9, 0.1);
+            glVertex3f(5.7, 3.9, 0.1); glVertex3f(6.0, 4.2, 0.1);
+            glVertex3f(6.0, 4.2, 0.1); glVertex3f(6.0, 4.6, 0.1);
+            glVertex3f(6.0, 4.6, 0.1); glVertex3f(5.7, 4.9, 0.1);
+            glVertex3f(5.7, 4.9, 0.1); glVertex3f(5.3, 4.9, 0.1);
+            glVertex3f(5.3, 4.9, 0.1); glVertex3f(5.1, 4.6, 0.1);
+            glVertex3f(5.1, 4.6, 0.1); glVertex3f(5.1, 4.2, 0.1);
+            glVertex3f(5.1, 4.2, 0.1); glVertex3f(5.3, 3.9, 0.1);
+        glEnd();
+        glBegin(GL_LINES);
+            glVertex3f(6.3, 3.9, 0.1); glVertex3f(6.7, 3.9, 0.1);
+            glVertex3f(6.7, 3.9, 0.1); glVertex3f(7.0, 4.2, 0.1);
+            glVertex3f(7.0, 4.2, 0.1); glVertex3f(7.0, 4.6, 0.1);
+            glVertex3f(7.0, 4.6, 0.1); glVertex3f(6.7, 4.9, 0.1);
+            glVertex3f(6.7, 4.9, 0.1); glVertex3f(6.3, 4.9, 0.1);
+            glVertex3f(6.3, 4.9, 0.1); glVertex3f(6.1, 4.6, 0.1);
+            glVertex3f(6.1, 4.6, 0.1); glVertex3f(6.1, 4.2, 0.1);
+            glVertex3f(6.1, 4.2, 0.1); glVertex3f(6.3, 3.9, 0.1);
+        glEnd();
+    glPopMatrix();
+}
---
+++ main.c
@@ -0,0 +1,294 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glut.h>
+#include <math.h>
+
+#define PI 3.141592654
+
+void InitScreen();
+void MovimentaMouse(int, int);
+void MouseFunc(int, int, int, int);
+void Keyboard(unsigned char, int, int);
+void SpecialKeys(int, int, int);
+void Timer(int);
+void TimerFunc(int );
+void Desenha();
+void Reshape(int, int);
+void Msg(char *, GLfloat , GLfloat );
+void DesenhaGrade();
+void DesenhaCarro();
+void DesenhaEstrada();
+void IdleFunc();
+
+GLfloat preto[]={0,0,0},        branco[]={1,1,1},           verde[]={0,1,0},            vermelho[]={1,0,0},
+        azul[]={0,0,1},         amarelo[]={1,1,0},          furchsia[]={1,0,1},         rosa[]={1,0.75,0.8},
+        rosaBri[]={1,0,0.5},    abobora[]={0.99,0.06,0.75}, ciano[]={0,1,1},            verdeEsc[]={0,0.39,0},
+        azulEsc[]={0,0,0.55},   marinho[]={0.07,0.04,0.56}, fuligem[]={0.24,0.17,0.12}, roxo[]={0.5,0,0.5},
+        jambo[]={1,0.27,0},     laranja[]={1,0.65,0},       marrom[]={0.65,0.16,0.16},  cinza[]={0.5,0.5,0.5},
+        cinzaClaro[]={0.7,0.7,0.7},
+        cinzaEsc[]={0.66,0.66,0.66},  azulCeu[]={0.53,0.81,0.98}, verdeGrama[]={0.49,0.99,0}, cinzaFosco[]={0.41,0.41,0.41};
+
+GLint sw, sh, winPosX, winPosY;
+GLfloat w_width, w_height;
+GLfloat mouseX, mouseY, mouseClickX, mouseClickY;
+GLfloat angX = 0, angY = 0, angZ = 0;
+GLdouble theta=90, aspect=1, d_near=25, d_far=500;
+GLdouble x_0=0, y_0=35.0, z_0=150,
+         x_ref=0, y_ref=0, z_ref=0,
+         V_x=0, V_y=1, V_z = 0,
+         xCam = 0, yCam= 0, zCam=0;
+GLdouble raio = 50;
+GLfloat coordX = 3.003, coordZ = 3.003, passo = 0.5, sinal = 1;
+GLfloat pos_car_x=0, pos_car_z = 105, viraCarro = 0;
+
+GLfloat passoEst = 0; GLboolean flagEst = 0; GLint fps = 60;
+
+void Msg(char *string, GLfloat x, GLfloat y){
+        glRasterPos2f(x,y);
+        while(*string)
+             glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12,*string++);
+}
+void FitWindow(GLsizei w, GLsizei h){
+     if (w >= h)
+        glutReshapeWindow(h, h);
+    else
+        glutReshapeWindow(w, w);
+}
+void IdleFunc(){
+    glutPostRedisplay();
+}
+void Reshape(int w, int h){
+    if (w >= h)
+        glViewport(0, 0, (GLsizei)h, (GLsizei)h) ;
+    else
+        glViewport(0, 0, (GLsizei)w, (GLsizei)w) ;
+    glMatrixMode(GL_PROJECTION) ;
+    glLoadIdentity() ;
+
+    InitScreen();
+}
+void InitScreen(){
+    w_width = glutGet(GLUT_WINDOW_WIDTH);
+    w_height = glutGet(GLUT_WINDOW_HEIGHT);
+
+    glMatrixMode(GL_PROJECTION); //define que a matrix é a de projeção
+    glLoadIdentity(); //carrega a matrix de identidade
+    gluPerspective(theta, aspect, d_near, d_far);
+
+    glPushMatrix();
+        glRotatef(angX, 1, 0, 0);
+        glRotatef(angY, 0, 1, 0);
+        glRotatef(angZ, 0, 0, 1);
+
+        glMatrixMode(GL_MODELVIEW); //define que a matrix é a model view
+        glLoadIdentity(); //carrega a matrix de identidade
+
+        gluLookAt(x_0, y_0, z_0,
+                  x_ref, y_ref, z_ref,
+                  V_x, V_y, V_z);
+    glPopMatrix();
+}
+
+void MovimentaMouse(int x, int y){
+    y = w_height - y;
+    mouseX = (double)(((double)y/(double)w_height)*2.0 - 1.0);
+    mouseY = (double)(((double)x/(double)w_width)*2.0 -1.0);
+
+}
+
+void MouseFunc(int botao, int estado, int x, int y){
+    switch (botao){
+        case GLUT_LEFT_BUTTON :
+            if (estado == GLUT_DOWN){
+                y = w_height - y;
+                mouseClickX = (float)(((float)x/(float)w_width)*2.0 -1.0);
+                mouseClickY = (float)(((float)y/(float)w_height)*2.0 - 1.0);
+            }
+            break ;
+        default :
+            break ;
+    }
+}
+void Keyboard (unsigned char key, int x, int y){
+    switch (key)    {
+    case 'x': angX += 1; break;
+    case 'y': angY += 1; break;
+    case 'z': angZ += 1; break;
+    case 'X': angX -= 1; break;
+    case 'Y': angY -= 1; break;
+    case 'Z': angZ -= 1; break;
+
+    case 'j':
+    case 'J':
+        pos_car_x -= 1;
+        viraCarro += 1;
+        break;
+    case 'l':
+    case 'L':
+        pos_car_x += 1;
+        viraCarro -= 1;
+        break;
+    case 'K': pos_car_z += 1;break;
+    case 'k': pos_car_z += 1;break;
+    case 'I': pos_car_z -= 1;break;
+    case 'i': pos_car_z -= 1;break;
+
+    case 'w': z_0 -= 1; break;
+    case 'W': z_0 -= 1; break;
+    case 's': z_0 += 1; break;
+    case 'S': z_0 += 1; break;
+    case 'a': x_0 -= 1; break;
+    case 'A': x_0 -= 1; break;
+    case 'd': x_0 += 1; break;
+    case 'D': x_0 += 1; break;
+    case 'e': y_0 += 1; break;
+    case 'E': y_0 += 1; break;
+    case 'q': y_0 -= 1; break;
+    case 'Q': y_0 -= 1; break;
+    case 'v': theta += 1; break;
+    case 'V': theta -= 1; break;
+    case 'f': aspect += 0.2; break;
+    case 'F': aspect -= 0.2; break;
+
+    case 'b':
+        flagEst = 0;
+        break;
+    case 'B':
+        flagEst = 1;
+        TimerFunc(0);
+        break;
+    default: break;
+    }
+    InitScreen();
+    printf("x_0: %g, y_0: %g, z_0: %g, theta: %g, aspect: %g \n", x_0, y_0, z_0, theta, aspect );
+    printf("pos_carX: %.2f - pos_carZ: %.2f\n", pos_car_x, pos_car_z);
+}
+void SpecialKeys (int key, int x, int y){
+   switch(key){
+        case GLUT_KEY_RIGHT:
+            d_far += 1;
+            break;
+        case GLUT_KEY_LEFT:
+            d_far -= 1;
+
+            break;
+        case GLUT_KEY_DOWN:
+            d_near -= 1 ;
+            break;
+        case GLUT_KEY_UP:
+            d_near += 1 ;
+            break;
+        case GLUT_KEY_PAGE_UP:
+            fps += 1;
+            break;
+        case GLUT_KEY_PAGE_DOWN:
+            fps -= 1;
+            break;
+        default:
+            break;
+    }
+    InitScreen();
+    printf("d_far: %.2f - d_near: %.2f - fps: %d\n", d_far, d_near, fps);
+}
+
+void TimerFunc(int value){
+    passoEst = passoEst < 9 ? passoEst + 1 : 0;
+    if(flagEst){
+        glutTimerFunc(1000/fps, TimerFunc, 1);
+        if(viraCarro > 0){
+            viraCarro = viraCarro * 0.96;
+        }else if (viraCarro < 0){
+            viraCarro = viraCarro + (0.4);
+        }
+    }
+    glutPostRedisplay();
+}
+void DesenhaEstrada(){
+    int i;
+    glColor3fv(verde);
+    for(i = -50; i <= 13; i++){
+    glPushMatrix();
+        glTranslatef( 97.5, 0, i*10 + passoEst);
+        glutSolidCube(5);
+    glPopMatrix();
+    glPushMatrix();
+        glTranslatef(-97.5, 0, i*10 + passoEst);
+        glutSolidCube(5);
+    glPopMatrix();
+    }
+
+}
+void DesenhaGrade(){
+    int i;
+    glBegin(GL_LINES);
+        glColor3fv(verde);
+        for(i = -10; i <= 10; i++){
+            glVertex3f(i *10, 0,  130);
+            glVertex3f(i *10, 0, -500);
+
+        }
+        for(i = -50; i <= 13; i++){
+            glVertex3f( 100, 0, i * 10 + passoEst);
+            glVertex3f(-100, 0, i * 10 + passoEst);
+        }
+    glEnd();
+    glLineWidth(4.0);
+    glBegin(GL_LINES);
+        glColor3fv(vermelho);
+        glVertex3f(0, 0,0);
+        glVertex3f(500, 0,0);
+    glEnd();
+    glBegin(GL_LINES);
+        glColor3fv(amarelo);
+        glVertex3f(0, 0,0);
+        glVertex3f(0, 500,0);
+    glEnd();
+    glBegin(GL_LINES);
+        glColor3fv(laranja);
+        glVertex3f(0, 0,0);
+        glVertex3f(0, 0,500);
+    glEnd();
+    glLineWidth(1.0);
+}
+void Desenha(){
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //limpa o buffer
+
+    DesenhaGrade();
+    DesenhaEstrada();
+    glPushMatrix();
+        glTranslatef(pos_car_x, 0, pos_car_z);
+        glTranslatef(0,0,-10);
+        glRotatef(viraCarro, 0, 1, 0);
+        glTranslatef(0,0, 10);
+        DesenhaCarro();
+    glPopMatrix();
+
+    glFlush();
+    glutSwapBuffers();
+}
+int main(int argc, char *argv[]){
+    glutInit(&argc, argv);
+
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    sw = glutGet(GLUT_SCREEN_WIDTH);
+    sh = glutGet(GLUT_SCREEN_HEIGHT);
+    winPosX = (sw-sh)/2; winPosY = 0;
+    glutInitWindowPosition(winPosX, winPosY);
+    glutInitWindowSize(sh, sh);
+    glutSetKeyRepeat(1);
+    glutCreateWindow("CG TRAB-FINAL - ENDURO");
+        FitWindow(glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT));
+        glutReshapeFunc(Reshape);
+        glutPassiveMotionFunc(MovimentaMouse);
+        glutMouseFunc(MouseFunc);
+        glutKeyboardFunc(Keyboard);
+        glutSpecialFunc(SpecialKeys);
+        glutTimerFunc(0,TimerFunc,1);
+        glutIdleFunc(IdleFunc);
+        glutDisplayFunc(Desenha);
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f); //define a cor de fundo
+        glEnable(GL_DEPTH_TEST); //habilita o teste de profundidade
+        InitScreen();
+    glutMainLoop();
+}
+
